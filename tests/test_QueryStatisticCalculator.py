bc107a64a7d7ba074a76ac4c9ea121ac5c1ca2b0
Alena Bartosh <51479635+alena-bartosh@users.noreply.github.com>
2019-11-01 23:46:27 +0200

    Add unittests

---
+++ tests/test_QueryStatisticCalculator.py
@@ -0,0 +1,87 @@
+import unittest
+from sys import maxsize as really_big_number
+import pandas as pd
+from query_statistic_calculator import QueryStatisticCalculator
+
+
+class QueryStatisticCalculatorTestCase(unittest.TestCase):
+    START_TIMESTAMP_IN_TEST_DATA = 1571356830600
+    FINISH_TIMESTAMP_IN_TEST_DATA = 1571356838845
+
+    def test_raise_exception_when_incorrect_dataframe(self):
+        with self.subTest(name='empty dataframe'):
+            empty_df = pd.DataFrame()
+
+            with self.assertRaises(ValueError):
+                query_statistic_calculator = QueryStatisticCalculator(empty_df)
+
+        with self.subTest(name='wrong column names in dataframe'):
+            df_with_wrong_column_name = pd.DataFrame(data={'query_id_WRONG': 1,
+                                                           'status': 2,
+                                                           'time': 3,
+                                                           'rows': 4,
+                                                           'threads': ['5,6,7']})
+
+            with self.assertRaises(ValueError):
+                query_statistic_calculator = QueryStatisticCalculator(df_with_wrong_column_name)
+
+        with self.subTest(name='wrong column types in dataframe'):
+            df_with_wrong_column_types = pd.DataFrame(columns=['query_id', 'status', 'time', 'rows', 'threads'])
+
+            with self.assertRaises(ValueError):
+                query_statistic_calculator = QueryStatisticCalculator(df_with_wrong_column_types)
+
+    def test_successful_creation_when_correct_dataframe(self):
+        df = pd.DataFrame(data={'query_id': 1,
+                                'status': 2,
+                                'time': 3,
+                                'rows': 4,
+                                'threads': ['5,6,7']})
+
+        query_statistic_calculator = QueryStatisticCalculator(df)
+
+    def test_raise_exception_when_timestamp_not_in_dataframe(self):
+        df = pd.read_csv('tests/queries_sample.tsv', sep='\t')
+
+        query_statistic_calculator = QueryStatisticCalculator(df)
+
+        with self.subTest(name='timestamp is less than minimal dataframe timestamp'):
+            with self.assertRaises(ValueError):
+                query_statistic_calculator.check_timestamp_in_df(1)
+
+        with self.subTest(name='timestamp is more than minimal dataframe timestamp'):
+            with self.assertRaises(ValueError):
+                query_statistic_calculator.check_timestamp_in_df(really_big_number)
+
+    def test_average_elapsed_time_is_calculated_correctly(self):
+        df = pd.read_csv('tests/queries_sample.tsv', sep='\t')
+
+        query_statistic_calculator = QueryStatisticCalculator(df)
+
+        average_elapsed_time = query_statistic_calculator.get_average_elapsed_seconds_on_date(
+            QueryStatisticCalculatorTestCase.START_TIMESTAMP_IN_TEST_DATA)
+        self.assertAlmostEqual(3.04475, average_elapsed_time)
+
+    def test_average_row_count_is_calculated_correctly(self):
+        df = pd.read_csv('tests/queries_sample.tsv', sep='\t')
+
+        query_statistic_calculator = QueryStatisticCalculator(df)
+
+        average_row_count = query_statistic_calculator.get_average_row_count(
+            QueryStatisticCalculatorTestCase.START_TIMESTAMP_IN_TEST_DATA,
+            QueryStatisticCalculatorTestCase.FINISH_TIMESTAMP_IN_TEST_DATA)
+        self.assertAlmostEqual(8.49, average_row_count, places=2)
+
+    def test_average_row_count_per_thread_is_calculated_correctly(self):
+        df = pd.read_csv('tests/queries_sample.tsv', sep='\t')
+
+        query_statistic_calculator = QueryStatisticCalculator(df)
+
+        average_row_count_per_thread = query_statistic_calculator.get_average_row_count_per_thread(
+            QueryStatisticCalculatorTestCase.START_TIMESTAMP_IN_TEST_DATA,
+            QueryStatisticCalculatorTestCase.FINISH_TIMESTAMP_IN_TEST_DATA)
+        self.assertAlmostEqual(5, average_row_count_per_thread)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_TimestampConverter.py
@@ -0,0 +1,67 @@
+import unittest
+from timestamp_converter import TimestampConverter
+
+
+class TimestampConverterTestCase(unittest.TestCase):
+    def test_timestamp_is_correctly_converted(self):
+        # 2019 Oct 19 01:36:39 UTC
+        timestamp = 1571448999
+        milliseconds = 423
+        timestamp_with_milliseconds = int(timestamp * TimestampConverter.MILLISECONDS_PER_SECOND + milliseconds)
+
+        dt = None
+
+        with self.subTest(name='get datetime from timestamp with milliseconds'):
+            dt = TimestampConverter.get_datetime_from_timestamp(timestamp_with_milliseconds)
+
+            self.assertEqual(2019, dt.date().year)
+            self.assertEqual(10, dt.date().month)
+            self.assertEqual(19, dt.date().day)
+
+            self.assertEqual(1, dt.time().hour)
+            self.assertEqual(36, dt.time().minute)
+            self.assertEqual(39, dt.time().second)
+            self.assertEqual(milliseconds * TimestampConverter.MICROSECONDS_PER_MILLISECOND, dt.time().microsecond)
+
+        with self.subTest(name='get timestamp from datetime with milliseconds'):
+            new_timestamp = TimestampConverter.get_timestamp_from_datetime(dt)
+
+            self.assertEqual(timestamp_with_milliseconds, new_timestamp)
+
+        timestamp_with_zeroed_time = None
+
+        with self.subTest(name='get timestamp with zeroed time'):
+            timestamp_with_zeroed_time = TimestampConverter.get_timestamp_with_zeroed_time(timestamp_with_milliseconds)
+
+            self.assertEqual(1571443200000, timestamp_with_zeroed_time)
+
+            new_dt = TimestampConverter.get_datetime_from_timestamp(timestamp_with_zeroed_time)
+
+            self.assertEqual(2019, new_dt.date().year)
+            self.assertEqual(10, new_dt.date().month)
+            self.assertEqual(19, new_dt.date().day)
+
+            self.assertEqual(0, new_dt.time().hour)
+            self.assertEqual(0, new_dt.time().minute)
+            self.assertEqual(0, new_dt.time().second)
+            self.assertEqual(0, new_dt.time().microsecond)
+
+        with self.subTest(name='get timestamp plus one day'):
+            timestamp_plus_one_day = TimestampConverter.get_timestamp_plus_one_day(timestamp_with_zeroed_time)
+
+            self.assertEqual(1571529600000, timestamp_plus_one_day)
+
+            new_dt = TimestampConverter.get_datetime_from_timestamp(timestamp_plus_one_day)
+
+            self.assertEqual(2019, new_dt.date().year)
+            self.assertEqual(10, new_dt.date().month)
+            self.assertEqual(20, new_dt.date().day)
+
+            self.assertEqual(0, new_dt.time().hour)
+            self.assertEqual(0, new_dt.time().minute)
+            self.assertEqual(0, new_dt.time().second)
+            self.assertEqual(0, new_dt.time().microsecond)
+
+
+if __name__ == '__main__':
+    unittest.main()
